28e00d54635d8905fff99bcd13b786949eacd150
Guillaume Tremblay <32100830+thecanadianroot@users.noreply.github.com>
2021-06-18 15:43:27 +0000

    Smelly file

---
+++ thissmells.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char** argv){
+  std::cout << "Somebody" << "Once" << "Told" << "Me" << "The" << "World" << "Is" << "Gonna" << "Roll" << "Me";
+  std::cout << "Somebody" << "Once" << "Told" << "Me" << "The" << "World" << "Is" << "Gonna" << "Roll" << "Me";
+  std::cout << "Somebody" << "Once" << "Told" << "Me" << "The" << "World" << "Is" << "Gonna" << "Roll" << "Me";
+  std::cout << "Somebody" << "Once" << "Told" << "Me" << "The" << "World" << "Is" << "Gonna" << "Roll" << "Me";
+  std::cout << "Somebody" << "Once" << "Told" << "Me" << "The" << "World" << "Is" << "Gonna" << "Roll" << "Me";
+  std::cout << "Somebody" << "Once" << "Told" << "Me" << "The" << "World" << "Is" << "Gonna" << "Roll" << "Me";
+  std::cout << "Somebody" << "Once" << "Told" << "Me" << "The" << "World" << "Is" << "Gonna" << "Roll" << "Me";
+}
