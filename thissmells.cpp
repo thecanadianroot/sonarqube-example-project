156bb205fab988420300d61fd01e9db0a4ea43d0
Guillaume Tremblay <32100830+thecanadianroot@users.noreply.github.com>
2021-06-18 17:19:38 +0000

    Update thissmells.cpp

--- thissmells.cpp
+++ thissmells.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
+void printStuff(){
+    int a = 69;
+    int b = 420;
+    std::cout << a + b;
+    std::cout << "Never gonna give you up" << endl;
+    std::cout << "Never gonna let you down" << endl;
+    std::cout << "And hurt you." << endl;
+}
+
 int main(int argc, char** argv){
     std::cout << "Somebody" << "Once" << "Told" << "Me" << "The" << "World" << "Is" << "Gonna" << "Roll" << "Me";
     std::cout << "Somebody" << "Once" << "Told" << "Me" << "The" << "World" << "Is" << "Gonna" << "Roll" << "Me";
