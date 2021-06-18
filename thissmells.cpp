c9bd68485aaf7431f3aaf059d4b4c5aa5e8f6c81
Guillaume Tremblay <32100830+thecanadianroot@users.noreply.github.com>
2021-06-18 15:45:38 +0000

    Update thissmells.cpp

--- thissmells.cpp
+++ thissmells.cpp
@@ -8,4 +8,5 @@ int main(int argc, char** argv){
   std::cout << "Somebody" << "Once" << "Told" << "Me" << "The" << "World" << "Is" << "Gonna" << "Roll" << "Me";
   std::cout << "Somebody" << "Once" << "Told" << "Me" << "The" << "World" << "Is" << "Gonna" << "Roll" << "Me";
   std::cout << "Somebody" << "Once" << "Told" << "Me" << "The" << "World" << "Is" << "Gonna" << "Roll" << "Me";
+  std::cout << "I ain't the sharpest tool in the shed";
 }
